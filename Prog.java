240010df7fed9f6b1adfeacf585b05461fb24aa6
Ali Azlan <aliazlan396@gmail.com>
2025-11-12 03:26:17 +0500

    The Second Commit

--- Prog.java
+++ Prog.java
@@ -1,4 +1,4 @@
-public class HelloJenkins {
+public class Prog {
     public static void main(String[] args) {
         System.out.println("🚀 Jenkins is successfully building this Java project!");
     }
